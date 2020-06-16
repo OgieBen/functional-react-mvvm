57ba98ed77f011bccaa968ac244d027262d868d1
Ben Ogie <bensoft2k5@gmail.com>
2020-06-16 10:42:36 +0200

    removed unused models

--- src/ui/components/mutable/clickcounter/viewmodel.ts
+++ src/ui/components/mutable/clickcounter/viewmodel.ts
@@ -3,27 +3,6 @@ import {decorate, observable} from "mobx";
 import {Subject} from "rxjs";
 import {Dispatch} from "react";
 
-export interface ILoginViewModel {
-    user: User;
-    clickCount: number;
-}
-
-export class LoginModel implements ILoginViewModel {
-    constructor(readonly user: User, readonly clickCount: number = 0) {
-    };
-}
-
-// User
-export interface IUser {
-    userName: String;
-    password: String;
-}
-
-export class User implements IUser {
-    constructor(readonly userName: String = "", readonly password: String = "",) {
-    };
-}
-
 // Click
 export interface IClick {
     counter: number;
--- src/ui/pages/app/App.tsx
+++ src/ui/pages/app/App.tsx
@@ -5,17 +5,6 @@ import {Click} from "../../components/mutable/clickcounter/viewmodel";
 import SimpleHeader from "../../components/mutable/simpleheader/simpleheader";
 
 function App() {
-    // fetch the latest view model here from data provider
-
-    // Avoid large view models like this as this will cause
-    // inefficient re-rendering of layouts even for
-    // components that do not depend on the latest
-    // view model updates
-    // const loginModel = new LoginModel(new User(), 0);
-
-    // instead break them into independent components like this
-    // const userModel = new User();
-    // const clickCounterModel = new Click(0);
     return (
         <div>
             <SimpleHeader counter={0}></SimpleHeader>
