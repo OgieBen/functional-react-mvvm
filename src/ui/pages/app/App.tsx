e1960afb88f33442dc2c15bcd651d1f6be5a79a0
Ben Ogie <bensoft2k5@gmail.com>
2020-06-16 10:39:33 +0200

    removed comments

--- src/ui/pages/app/App.tsx
+++ src/ui/pages/app/App.tsx
@@ -13,17 +13,6 @@ import {observer} from "mobx-react";
 const viewModel = ClickCounterViewModelMutator.getViewModel();
 
 function App() {
-    // fetch the latest view model here from data provider
-
-    // Avoid large view models like this as this will cause
-    // inefficient re-rendering of layouts even for
-    // components that do not depend on the latest
-    // view model updates
-    // const loginModel = new LoginModel(new User(), 0);
-
-    // instead break them into independent components like this
-    // const userModel = new User();
-    // const clickCounterModel = new Click(0);
     return (
         <React.Fragment>
             <Header viewModel={viewModel}></Header>
